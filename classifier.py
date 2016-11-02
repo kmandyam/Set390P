58f2cb2249149ce33d02660dec79b62d66cbcc67
Ayush Saraf <ayush29f@attu2.cs.washington.edu>
2016-11-01 17:31:59 -0700

    first shape contour working

--- classifier.py
+++ classifier.py
@@ -11,14 +11,13 @@ ret,img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
 
 # Find Contours
 img, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours_ = []
-hierarchy_ = []
-for c, h in zip(contours, hierarchy):
-  if cv2.contourArea(c) > 50:
-    contours_.append(c)
-    hierarchy_.append(h)
+contours_ = [c for c, h in zip(contours, hierarchy[0]) if h[3] == 0]
 
-hierarchy_ = [list(x) for x in hierarchy_[0] if x[3] == 0]
-count_ = len(hierarchy_)
+count_ = len(contours_)
 print(count_)
+
+whole_shape = np.zeros(img.shape, np.uint8)
+cv2.drawContours(whole_shape, [contours_[0]], 0, 255, -1)
+cv2.drawContours(whole_shape, [contours_[0]], 0, 0, 2)
+cv2.imwrite('out/contours.png', whole_shape)
 cv2.imwrite('out/card1.png', img)
