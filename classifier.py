75942b7a153014fccc0b5d1c9eec0cb91413fd7c
Ayush Saraf <ayush29f@attu2.cs.washington.edu>
2016-11-01 17:11:42 -0700

    count done

--- classifier.py
+++ classifier.py
@@ -1,17 +1,24 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('data/card1.png')
+original_img = cv2.imread('data/card7.png')
 
 # Process Image
 kernel = np.ones((2,2), np.uint8)
-img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+img = cv2.morphologyEx(original_img, cv2.MORPH_OPEN, kernel)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret,img = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
 
 # Find Contours
 img, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-print(hierarchy)
-hierarchy = [list(x) for x in hierarchy[0] if x[3] == 0]
-print(hierarchy)
+contours_ = []
+hierarchy_ = []
+for c, h in zip(contours, hierarchy):
+  if cv2.contourArea(c) > 50:
+    contours_.append(c)
+    hierarchy_.append(h)
+
+hierarchy_ = [list(x) for x in hierarchy_[0] if x[3] == 0]
+count_ = len(hierarchy_)
+print(count_)
 cv2.imwrite('out/card1.png', img)
